ef0089265b4b2b57f19a03683ad01cfe256db08a
ʍօղíɾ հօՏՏɑíղ <60436021+botolmonir@users.noreply.github.com>
2020-10-17 19:23:57 +0600

    Create Banner.sh

---
+++ Banner.sh
@@ -0,0 +1,24 @@
+NBlue='\033[0;34m'
+NC='\033[0m'
+Red='\033[1;31m'
+Yellow='\033[1;33m'
+Green='\033[1;32m'
+Blue='\033[1;34m'
+Cyan='\033[1;36;40m'
+Gray='\033[1;30;40m'
+White='\033[1;37;40m'
+nta='\033[1;35;40m'
+
+echo -e "$Blue   _   _   _   _   _   _   _   _  $Green  B
+$Blue. / \ / \ / \ / \ / \ / \ / \ / \ $Red  A
+$Blue ( P | A | S | S | W | O | R | D ) $Yellow B
+$Blue  \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ $Gray  A
+
+$Blue————————————————————————————————————                  $Green v0.1"                          
+echo -e  "$Green @$Red Created by$Green B $Yellow O $Cyan T $Red O $nta L  $Green B $Yellow A $Cyan B $Red A
+$Yellow @$NBlue https://$Cyan www.facebook.com$Blue /monir.hossain.8
+$Cyan @$Gray https://$nta github.com$Red /botolmonir
+$Red @$Blue TOOL : $Green PASSWORD LIST MAKER $nta [][] $Green [][] $Yellow [][] $Cyan [][] $Red [][]
+$Blue————————————————————————————————————
+$Green"
+
